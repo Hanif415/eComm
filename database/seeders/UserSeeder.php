178a0d2799cd4880eff1147d3e329b8abdbea403
hanif415 <hanifamrullah42@gmail.com>
2022-12-15 12:27:59 +0700

    commmit

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -16,9 +16,9 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Hanif',
-            'email' => 'hanif@gmail.com',
-            'password' =>Hash::make('12345');
+            'name' => 'Peters',
+            'email' => 'peterparker@gmail.com',
+            'password' => Hash::make('12345')
         ]);
     }
 }
